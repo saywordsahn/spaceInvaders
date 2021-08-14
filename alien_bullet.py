5dcdffc79f0976e0f9f5ba4c5bed0ecd53a8d80c
saywordsahn <saywordsahn@gmail.com>
2021-08-14 09:45:06 +0900

    Added AlienBullet class

--- alien_bullet.py
+++ alien_bullet.py
@@ -2,8 +2,17 @@ import pygame
 
 class AlienBullet(pygame.sprite.Sprite):
 
-    def __init__(self, image, x, y):
+    def __init__(self, image, x, y, explode_fx):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.explode_fx = explode_fx
+
+    def update(self, screen_height, player_group):
+        self.rect.y += 2
+        if self.rect.top > screen_height:
+            self.kill()
+        if pygame.sprite.spritecollide(self, player_group, False, pygame.sprite.collide_mask):
+            self.kill()
+            self.explode_fx.play()
